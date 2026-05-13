43b4a8a70117bedb6ff07dbde09bdc482d6849df
jshowker <heufhrbvbirb@gmail.com>
2026-05-13 15:14:25 +0200

    Update site configuration and content for personal portfolio. Set output to static in astro.config.mjs, change project name in package-lock.json, and update global constants with new site title and description. Revamp header and footer components for improved layout and styling, and enhance the homepage with a hero section showcasing 3D art and commission rates. Adjust global styles for a dark, minimal aesthetic.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://example.com",
+	output: "static",
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Jshowkerr";
+export const SITE_DESCRIPTION =
+	"Experienced 3D artist specialized in stylized and hand-painted 3D models.";
+
+export const DISCORD_URL = "https://t.co/abelCpD5Ky";
+export const TWITTER_URL = "https://x.com/jshowkerr";
